c9135cefe25dff72604e8f88c8f59b593200bc10
HanzilaH <120693000+HanzilaH@users.noreply.github.com>
2023-09-17 21:48:45 +0100

    removed extra imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,13 @@
 import './App.css';
 import Home from './pages/Home';
-import { DataContextProvider } from './context/DataContext';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Subjectstore from './components/Subjectstore';
-import Subjectview from './components/Subjectview';
-import QuestionStore from './components/QuestionStore';
-import Subjectflash from './components/Subjectflash';
 import { useContext, useEffect, useState } from 'react';
-import Subjectlist from './components/Subjectlist';
-import Subject from './pages/Subject';
-import Navbar from './components/Navbar';
-import SignIn from './components/auth/SignIn';
-import SignUp from './components/auth/SignUp';
 import Authenticate from './pages/Authenticate';
-import { db } from './firebase/firebaseConfig';
-import { collection, getDocs } from 'firebase/firestore';
-import { faAsterisk } from '@fortawesome/free-solid-svg-icons';
-import { useAuth } from './context/AuthContext';
-import DataContext from './context/DataContext';
+
 
 
 function App() {
 
-  const {currentUser, logout} = useAuth()
-  const {userData} = useContext(DataContext) 
-  const [users, setUsers] = useState([]);
-  const usersCollectionRef = collection(db, 'users');
   
 
   useEffect(() => {
